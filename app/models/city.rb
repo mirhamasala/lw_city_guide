5e16e1d863b43ecf31e0fd142eeea45eda1c7c1b
Mirha Masala <mirha.masala@me.com>
2019-07-07 11:10:56 +0800

    Add associations and validations

--- app/models/city.rb
+++ app/models/city.rb
@@ -1,2 +1,6 @@
 class City < ApplicationRecord
+  has_many :spots
+
+  validates :name, presence: true, uniqueness: true
+  validates :country, :photo_url, presence: true
 end
--- app/models/spot.rb
+++ app/models/spot.rb
@@ -1,3 +1,8 @@
 class Spot < ApplicationRecord
   belongs_to :city
+
+  validates :name, :category, :description, :address, :photo_url, :city_id, presence: true
+  validates :name, uniqueness: { scope: :address }
+  # validates :name, uniqueness: { scope: :address,
+    # message: "already exists on this address" }
 end
