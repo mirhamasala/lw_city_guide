b9a8414bfeb32b0718197901f5c4878641cb2697
Mirha Masala <mirha.masala@me.com>
2019-07-08 13:17:08 +0800

    Add new spot view

--- app/controllers/spots_controller.rb
+++ app/controllers/spots_controller.rb
@@ -1,13 +1,38 @@
 class SpotsController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:show]
+  skip_before_action :authenticate_user!, only: [:new, :create, :show]
   before_action :set_spot, only: [:show]
 
+  def new
+    @city = City.find_by_name("Barcelona")
+    @spot = Spot.new
+  end
+
+  def create
+    @user = User.find(1)
+    @city = City.find(params[:city_id])
+    @spot = Spot.new(spot_params)
+    @spot.user = @user
+    @spot.city = @city
+    @spot.save
+
+    if @spot.save
+      redirect_to @spot
+    else
+      render :new
+    end
+  end
+
   def show
+    @city = City.find_by_name("Barcelona")
   end
 
   private
 
   def set_spot
     @spot = Spot.find(params[:id])
+  end
+
+  def spot_params
+    params.require(:spot).permit(:name, :category, :sub_category, :description, :address, :latitude, :longitude, :phone_number, :website, :photo)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "cities#home"
 
-  resources :cities, only: [:show]
+  resources :cities, only: [:show] do
+    resources :spots, only: [:new, :create]
+  end
   resources :spots, only: [:show, :destroy]
 end
