be98fe22ab17d3c667a59bd0ab196e7d54f3c0cf
Mirha Masala <mirha.masala@me.com>
2019-07-09 19:53:58 +0800

    Update seed, add Madrid

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
-puts "Wiping out the city..."
+puts "Wiping out civilizations..."
 City.destroy_all
 Spot.destroy_all
 User.destroy_all
@@ -8,7 +8,7 @@ User.create!(email: "mirha@testing.com", password: "testing", admin: true)
 User.create!(email: "g@testing.com", password: "testing", admin: true)
 User.create!(email: "gm@testing.com", password: "testing")
 
-puts "Building Romes in less than a day..."
+puts "Building Spanish Romes in less than a day..."
 city_attributes = [
   {
     name: "Barcelona",
@@ -16,19 +16,18 @@ city_attributes = [
     # remote_photo_url: "https://images.unsplash.com/photo-1507619579562-f2e10da1ec86"
   },
   {
-    name: "Canggu",
-    country: "Indonesia"
-    # remote_photo_url: "https://images.unsplash.com/photo-1518982380512-5a3c6d7118c4"
+    name: "Madrid",
+    country: "Spain"
   }
 ]
 
 City.create!(city_attributes)
-puts "Put Barcelona & Canggu on the map..."
+puts "Pinned Barcelona & Madrid on the map..."
 
 puts "Hunting hotspots..."
 spot_attributes = [
   {
-    name: "Le Wagon",
+    name: "Le Wagon Barcelona",
     category: "other",
     sub_category: "Coding Bootcamp · 🚀",
     description: "Change your life, learn to code.",
@@ -64,6 +63,96 @@ spot_attributes = [
     # remote_photo_url: "https://media.timeout.com/images/100677963/630/472/image.jpg",
     city_id: 1,
     user_id: 1
+  },
+    {
+    name: "NAP Antic",
+    category: "eat",
+    sub_category: "Pizza · Real Deal",
+    description: "Neapolitan dough like it should be.",
+    address: "Avinguda de Francesc Cambó 30",
+    neighborhood: "Ciutat Vella",
+    phone_number: "+34 686 19 26 90",
+    website: "https://www.facebook.com/nap.pizzeria/",
+    city_id: 1,
+    user_id: 1
+  },
+    {
+    name: "Dry Martini",
+    category: "drink",
+    sub_category: "Cocktails · Shaken & Stirred",
+    description: "No guns allowed.",
+    address: "Carrer d'Aribau 162",
+    neighborhood: "Eixample",
+    phone_number: "+34 932 17 50 72",
+    website: "http://www.drymartiniorg.com/",
+    # remote_photo_url: "",
+    city_id: 1,
+    user_id: 1
+  },
+    {
+    name: "Boqueria Market",
+    category: "shop",
+    sub_category: "Market · Veggies & More",
+    description: "Nothing like filling your picnic basket at Boqueria.",
+    address: "La Rambla 89",
+    neighborhood: "Ciutat Vella",
+    phone_number: "+34 933 18 25 84",
+    website: "http://www.boqueria.info",
+    # remote_photo_url: "",
+    city_id: 1,
+    user_id: 1
+  },
+  {
+    name: "Le Wagon Madrid",
+    category: "other",
+    sub_category: "Coding Bootcamp · 🚀",
+    description: "Change your life, learn to code.",
+    address: "Calle de Juan de Mariana 15",
+    neighborhood: "Atocha",
+    phone_number: "+34 646 85 03 58",
+    website: "https://www.lewagon.com/madrid",
+    # remote_photo_url: "",
+    city_id: 2,
+    user_id: 2
+  },
+  {
+    name: "Café Belén",
+    category: "drink",
+    sub_category: "Mojitos · Minty",
+    description: "Square atmosphere and local hangs.",
+    address: "Calle de Belén 5",
+    neighborhood: "Chueca",
+    phone_number: "+34 913 08 27 47",
+    website: "http://www.elcafebelen.com/",
+    # remote_photo_url: "",
+    city_id: 2,
+    user_id: 2
+  },
+    {
+    name: "Chocolatería San Ginés",
+    category: "eat",
+    sub_category: "Porras · Better Donuts",
+    description: "Life before keto: porras dunked in chocolate.",
+    address: "Pasadizo de San Ginés 5",
+    neighborhood: "Sol",
+    phone_number: "+34 913 65 65 46",
+    website: "https://www.chocolateriasangines.com/",
+    # remote_photo_url: "",
+    city_id: 2,
+    user_id: 2
+  },
+    {
+    name: "Mercado De San Miguel",
+    category: "shop",
+    sub_category: "Market · Vintage Deal",
+    description: "The Boqueria never stood a chance.",
+    address: "Plaza de San Miguel 5",
+    neighborhood: "Centro",
+    phone_number: "+34 915 42 49 36",
+    website: "http://www.mercadodesanmiguel.es/",
+    # remote_photo_url: "",
+    city_id: 2,
+    user_id: 2
   }
 ]
 
