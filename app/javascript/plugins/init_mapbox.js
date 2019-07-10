41609313dcabcdcec41c85459c774580b2e57249
Mirha Masala <mirha.masala@me.com>
2019-07-10 12:17:02 +0800

    Fit map to markers

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -3,6 +3,12 @@ import mapboxgl from 'mapbox-gl';
 const initMapbox = () => {
   const mapElement = document.getElementById('map');
 
+  const fitMapToMarkers = (map, markers) => {
+    const bounds = new mapboxgl.LngLatBounds();
+    markers.forEach(marker => bounds.extend([marker.lng, marker.lat]));
+    map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 });
+  };
+
   if (mapElement) { // only build a map if there's a div#map to inject into
     mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
     const map = new mapboxgl.Map({
@@ -16,6 +22,9 @@ const initMapbox = () => {
         .setLngLat([marker.lng, marker.lat])
         .addTo(map);
     });
+
+    fitMapToMarkers(map, markers);
+
   }
 };
 
