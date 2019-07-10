13b34a411fe0066267d74b387ba93e11ef5b0143
Mirha Masala <mirha.masala@me.com>
2019-07-10 12:07:48 +0800

    Fix typo

--- app/models/spot.rb
+++ app/models/spot.rb
@@ -1,5 +1,5 @@
 class Spot < ApplicationRecord
-  gstgeocoded_by :address
+  geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
   mount_uploader :photo, PhotoUploader
 
