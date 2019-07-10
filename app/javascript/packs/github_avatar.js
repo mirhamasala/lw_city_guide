5f3c636c89032ace2f203bddc9fd5a3bc7355123
Mirha Masala <mirha.masala@me.com>
2019-07-10 16:44:50 +0800

    Fix avatar js for when user not logged in

--- app/javascript/packs/github_avatar.js
+++ app/javascript/packs/github_avatar.js
@@ -1,8 +1,8 @@
-const userHandle = document.querySelector("[data-handle]").dataset.handle;
+const userHandle = document.querySelector("[data-handle]");
 
 const getAvatar = () => {
   const avatar = document.querySelector(".avatar");
-  const url = `https://api.github.com/users/${userHandle}`;
+  const url = `https://api.github.com/users/${userHandle.dataset.handle}`;
   fetch(url).then(response => response.json())
   .then((data) => {
     avatar.src = data.avatar_url;
