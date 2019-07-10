a9d6c5f0ffdaf5ae9dd2c8e4f9081151469d497a
Mirha Masala <mirha.masala@me.com>
2019-07-10 12:16:34 +0800

    Add markers

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -9,6 +9,13 @@ const initMapbox = () => {
       container: 'map',
       style: 'mapbox://styles/mapbox/streets-v10'
     });
+
+    const markers = JSON.parse(mapElement.dataset.markers);
+    markers.forEach((marker) => {
+      new mapboxgl.Marker()
+        .setLngLat([marker.lng, marker.lat])
+        .addTo(map);
+    });
   }
 };
 
