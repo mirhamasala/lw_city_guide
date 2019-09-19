31d0c86024cb858437ec542ce6d995524f878784
Mirha Masala <mirha.masala@me.com>
2019-09-19 15:09:35 +0800

    Move logic to see all spots to spots controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "cities#index"
   resources :cities, only: [:index, :show] do
-    resources :spots, only: [:new, :create]
+    resources :spots, only: [:index, :new, :create]
   end
   resources :spots, only: [:show, :edit, :update, :destroy] do
     resources :ratings, only: [:create, :update]
