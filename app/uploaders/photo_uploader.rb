2dee51b767fbb4e8c677711dbe7b8af28dba41f9
Mirha Masala <mirha.masala@me.com>
2019-10-15 18:07:52 +0800

    Add photo previews for forms

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -2,4 +2,8 @@ class PhotoUploader < CarrierWave::Uploader::Base
   include Cloudinary::CarrierWave
 
   process resize_to_limit: [1800, 1800]
+
+  version :preview do
+    resize_to_fit 192, 256
+  end
 end
