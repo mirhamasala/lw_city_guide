7ff4c949c57fda2a6dd53dc730736f79c1795754
Mirha Masala <mirha.masala@me.com>
2019-10-16 17:25:48 +0800

    Add thumbnail version

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -1,7 +1,13 @@
 class PhotoUploader < CarrierWave::Uploader::Base
   include Cloudinary::CarrierWave
 
+  process eager: true
+
   process resize_to_limit: [1800, 1800]
+
+  version :thumbnail do
+    resize_to_fit 256, 256
+  end
 
   version :preview do
     resize_to_fit 192, 256
