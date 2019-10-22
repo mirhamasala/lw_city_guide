f8da58d627e8eca370c2ddec56a613d96bff4f0f
Mirha Masala <mirha.masala@me.com>
2019-10-22 17:13:43 +0800

    Create images for responsive design

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -5,11 +5,20 @@ class PhotoUploader < CarrierWave::Uploader::Base
 
   process resize_to_limit: [1800, 1800]
 
+  # version :standard do
+  #   cloudinary_transformation :quality => :auto,
+  #                             :fetch_format => :auto,
+  #                             :width => :auto,
+  #                             :crop => :scale,
+  #                             :dpr => :auto,
+  #                             :responsive => :true
+  # end
+
   version :thumb do
-    resize_to_fit 256, 256
+    resize_to_fit(256, 256)
   end
 
   version :preview do
-    resize_to_fit 192, 256
+    resize_to_fit(192, 256)
   end
 end
