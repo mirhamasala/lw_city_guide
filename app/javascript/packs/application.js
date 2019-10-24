389c5fb03978eca4cb043b3afb082705860ea85d
Mirha Masala <mirha.masala@me.com>
2019-10-24 14:10:45 +0800

    Move get avatar

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,7 +1,7 @@
 import "bootstrap";
 import { ratingSlider } from "../components/rating_slider";
 import { initMapbox } from "../plugins/init_mapbox";
-import { initGetAvatar } from "./github_avatar";
+import { initGetAvatar } from "../components/github_avatar";
 import { initAutocomplete } from "../plugins/init_autocomplete";
 import { initCloudinary } from "../plugins/init_cloudinary";
 
