e38b7fc22277b572b4a6805af35bc745ae13caaf
Mirha Masala <mirha.masala@me.com>
2019-11-07 15:04:46 +0800

    Create filter overlay

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,9 +4,11 @@ import { initMapbox } from "../plugins/init_mapbox";
 import { initGetAvatar } from "../components/github_avatar";
 import { initAutocomplete } from "../plugins/init_autocomplete";
 import { initCloudinary } from "../plugins/init_cloudinary";
+import { initFilter } from "../components/filter";
 
 document.addEventListener("DOMContentLoaded", initGetAvatar);
 
 initMapbox();
 initAutocomplete();
 initCloudinary();
+initFilter();
