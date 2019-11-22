56ee489ea3373cab2f1cfe27f11863e2a85cc140
Farhan Ahsan <bbss1e3farhan@gmail.com>
2019-11-22 17:31:07 +0800

    merge error

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2019_11_17_085849) do
-
+ActiveRecord::Schema.define(version: 2019_11_18_035016) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
