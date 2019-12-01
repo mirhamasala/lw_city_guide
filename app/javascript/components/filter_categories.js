292a07a212e8047094348dc0f6b33ea62c44492c
Kaiying Fu <kaiying.fu@gmail.com>
2019-12-01 23:06:46 +0800

    AJAXify filter. Demonstrate AJAX filter under original filter mechanism. No CSS on filter buttons to highlight selection

--- app/javascript/components/filter_categories.js
+++ app/javascript/components/filter_categories.js
@@ -1,21 +1,48 @@
-// const getCategories = () => {
-//   const categoriesJSON = document.querySelector('[data-categories]').dataset['categories'];
-//   return JSON.parse(categoriesJSON);
-// }
-
-const getSelectedCategoryName = () => {
-  const categories = document.querySelectorAll('.categories-list__item');
-  categories.forEach((category) => {
-    category.addEventListener('click', (event) => {
-      const selectedCategory = event.currentTarget;
-      return selectedCategory.dataset['name'];
+// Returns IDs of selected categories as array of strings
+const filterCategories = (categories) => {
+  let selectedCategoryIds = [];
+  categories.forEach(category => {
+    category.addEventListener("click", (event) => {
+      const selectedCategoryId = parseInt(event.currentTarget.dataset["id"]);
+      if (selectedCategoryIds.includes(selectedCategoryId)) {
+        selectedCategoryIds = selectedCategoryIds.filter(id => id != selectedCategoryId);
+      } else {
+        selectedCategoryIds.push(selectedCategoryId);
+      }
+      console.log(selectedCategoryIds);
+      filterSpots(selectedCategoryIds);
     });
   });
 }
 
-const filterByCategories = (selectedCategoryName) => {
-  const spotsRender = document.getElementById('spots-render');
-  // spotsRender.innerHTML = `<%= render @spots.in_category(${selectedCategoryName}) %>`
+
+const filterSpots = (selectedCategoryIds) => {
+  console.log("now it works a little bit more");
+  // filter spots and insert html for each spot into #spot__card
+  const spotsDiv = document.getElementById("spot__card");
+  spotsDiv.innerHTML = '';
+  const spotsJSON = document.querySelector("[data-spots]");
+  const spots = JSON.parse(spotsJSON.dataset["spots"]);
+  const filteredSpots = spots.filter(spot => {
+    console.log("and... a little bit more");
+    return selectedCategoryIds.includes(spot.category_id);
+  });
+  filteredSpots.forEach(spot => {
+
+    const spotCodeStart = "<div class='spots__card-wrapper'><div class='spot-card'>";
+    const spotLink = `<a class="spot-card__link-wrapper" href="/spots/${spot.id}">`;
+    const spotImg = `<img class="spot-card__image" src="${spot.photo['url']}">`;
+    const spotText = "<div class='spot-card__text'>";
+    const spotSubCat = `<p class="spot-card__sub-category">${spot.sub_category}</p>`;
+    const spotHeader = `<p class="spot-card__header">${spot.name}</p>`;
+    const spotCodeEnd = "</div></a></div></div>";
+
+    const spotCode = spotCodeStart + spotLink + spotImg + spotText + spotSubCat + spotHeader + spotCodeEnd;
+    spotsDiv.insertAdjacentHTML("afterbegin", spotCode);
+  });
 }
 
-export { getSelectedCategoryName, filterByCategories };
+export { filterCategories }
+
+
+
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,12 +6,19 @@ import { initAutocomplete } from "../plugins/init_autocomplete";
 import { initCloudinary } from "../plugins/init_cloudinary";
 import ToggleMapView from "../components/toggle_map_view";
 import Filters from "../components/filters";
+import { filterCategories } from "../components/filter_categories";
 
 document.addEventListener("DOMContentLoaded", initGetAvatar);
 
 initMapbox();
 initAutocomplete();
 initCloudinary();
+
+const allCategories = document.querySelectorAll("[data-id]");
+if (allCategories) {
+  console.log("it kinda works");
+  filterCategories(allCategories);
+}
 
 new ToggleMapView();
 new Filters();
