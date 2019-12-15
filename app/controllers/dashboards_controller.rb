bab36503a3b585ec075cefd03b38b69965fcd459
Mirha Masala <mirha.masala@me.com>
2019-12-15 14:13:54 +0800

    Add size of pending reviews

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -3,6 +3,7 @@ class DashboardsController < ApplicationController
     authorize :dashboard
     @spots = Spot.author?(current_user)
     @cities = City.for(current_user)
-    @unpublished_spots_by_city = Spot.draft.for_user(current_user).group_by(&:city).sort_by(&:first)
+    @unpublished_spots = Spot.draft.for_user(current_user)
+    @unpublished_spots_by_city = @unpublished_spots.group_by(&:city).sort_by(&:first)
   end
 end
