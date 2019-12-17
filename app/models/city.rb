1888ba6f2bef4ae9ea6bc2b76636973fc7b4bc40
Mirha Masala <mirha.masala@me.com>
2019-12-17 13:57:33 +0800

    Remove cover validation

--- app/models/city.rb
+++ app/models/city.rb
@@ -6,7 +6,7 @@ class City < ApplicationRecord
 
   validates :name, presence: true, uniqueness: true
   validates :country, presence: true
-  validates :cover, presence: true
+  # validates :cover, presence: true
   validates :keepers, presence: true
 
   def self.alphabetize
