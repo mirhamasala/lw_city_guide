4d84d3c4f1b8862c417a2d42f10df725ba8e8403
Mirha Masala <mirha.masala@me.com>
2019-12-18 15:32:04 +0800

    Config testing environment

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,6 +5,15 @@ require 'rails/test_help'
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
+  include Warden::Test::Helpers
+  Wardern.test_mode!
 
   # Add more helper methods to be used by all tests here...
 end
+
+Capybara.register_driver :headless_chrome do |app|
+  options = Selenium::WebDriver::Chrome::Options.new(args: %w[no-sandbox headless disable-gpu window-size=1400,900])
+  Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
+end
+Capybara.save_path = Rails.root.join('tmp/capybara')
+Capybara.javascript_driver = :headless_chrome
