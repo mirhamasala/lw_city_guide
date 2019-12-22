5214729a472435466215c5eff904773390878a09
Farhan Ahsan <bbss1e3farhan@gmail.com>
2019-12-22 14:07:49 +0800

    changes made

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -6,12 +6,11 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     user = User.find_for_github_oauth(request.env['omniauth.auth'])
     if user == false
       session['devise.github_data'] = request.env['omniauth.auth']
-      set_flash_message(:alert, :not_a_wagon_student, kind: 'Github')
+      set_flash_message(:alert, kind: 'Github')
       redirect_to new_user_registration_url
     elsif user.persisted?
       set_flash_message(:notice, :success, kind: 'Github') if is_navigational_format?
       sign_in_and_redirect user, event: :authentication
-      # user.first_login ? redirect_to(edit_user_registration_path) : redirect_to(root_path)
     else
       session['devise.github_data'] = request.env['omniauth.auth']
       redirect_to new_user_registration_url
--- app/models/user.rb
+++ app/models/user.rb
@@ -19,33 +19,9 @@ class User < ApplicationRecord
     user_params[:github_picture_url] = auth.info.image
     user_params[:token] = auth.credentials.token
     user_params = user_params.to_h
-
-    repositories_json = open(auth.extra.raw_info.repos_url,
-      "Accept" => "application/vnd.github.v3+json",
-      "Authorization" => "token #{auth.credentials.token}"
-    ).read
-    repositories = JSON.parse(repositories_json)
-
-    # Identify all the repositories' names of the user
-    user_repos = repositories.map {|repo| repo["name"]}
-
-    # Array containing all the repositories of a LW students
-    lw_repos = [
-      "fullstack-challenges", "rails-stupid-coaching", "dotfiles",
-      "rails-longest-word-game", "rails-mister-cocktail", "rails-task-manager",
-      "rails-yelp-mvp", "rails-wikinimous"
-    ]
+    
     user = User.find_by(provider: auth.provider, uid: auth.uid)
     user ||= User.find_by(email: auth.info.email) # User did a regular sign up in the past.
-    if user
-      user.update(user_params)
-    elsif (lw_repos & user_repos).length > 1 # Check if the user has at least one LW student repo
-      user = User.new(user_params)
-      user.password = Devise.friendly_token[0,20]  # Fake password for validation
-      user.save
-    else
-      return false
-    end
 
     return user
   end
