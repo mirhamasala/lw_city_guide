54eb77cb2e1a74c884eb77a43b8c08bfb1d82e2f
Thomas Starzynski <thomas.starzynski@unibas.ch>
2020-01-04 05:05:41 +0100

    remove unneeded empty test template

--- test/controllers/spot_autocompletes_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class SpotAutocompletesControllerTest < ActionDispatch::IntegrationTest
-  test "should get run" do
-    get spot_autocompletes_run_url
-    assert_response :success
-  end
-
-end
