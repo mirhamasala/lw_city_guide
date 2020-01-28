608e4045315796934cfa61d0ce43b12531f077ee
Mirha Masala <mirha.masala@me.com>
2020-01-28 20:54:24 +0800

    Alphabetize unpublished spots in controller and add pusblished spots

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -3,7 +3,8 @@ class DashboardsController < ApplicationController
     authorize :dashboard
     @spots = Spot.author?(current_user)
     @cities = City.for(current_user)
-    @unpublished_spots = Spot.draft.for_user(current_user)
+    @published_spots = Spot.published.alphabetize.for_user(current_user)
+    @unpublished_spots = Spot.draft.alphabetize.for_user(current_user)
     @unpublished_spots_by_city = @unpublished_spots.group_by(&:city).sort_by(&:first)
     @categories = Category.all.alphabetize
     @countries = Country.all.alphabetize
