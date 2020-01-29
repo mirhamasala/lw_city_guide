f881f82a0faa7a8a3bffe71ae1f0f4dbc2dbaa4d
Mirha Masala <mirha.masala@me.com>
2020-01-29 20:18:19 +0800

    Alphabetize cities

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -2,7 +2,7 @@ class DashboardsController < ApplicationController
   def show
     authorize :dashboard
     @spots = Spot.author?(current_user)
-    @cities = City.for(current_user)
+    @cities = City.alphabetize.for(current_user)
     @published_spots = Spot.published.alphabetize.for_user(current_user)
     @unpublished_spots = Spot.draft.alphabetize.for_user(current_user)
     @unpublished_spots_by_city = @unpublished_spots.group_by(&:city).sort_by(&:first)
