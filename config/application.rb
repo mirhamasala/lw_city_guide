e3e83ed2103756fd500933e84aba5fa137ea45c7
Mirha Masala <mirha.masala@me.com>
2020-02-08 14:12:21 +0800

    User custom routes for error handling

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,8 @@ module LwCityGuide
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults "6.0"
 
+    config.exceptions_app = self.routes
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
