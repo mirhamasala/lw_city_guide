666d65b122dbf5147dcdbde37f4ca86a58a2c472
Mirha Masala <mirha.masala@me.com>
2020-02-13 08:19:35 +0800

    notes

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -26,11 +26,30 @@ class DashboardsController < ApplicationController
     end
 
     def published
-      @published ||= spots.published.group_by(&:city).sort_by(&:first)
+      # If @published has something, use it
+      # If not assign it spots.published
+      # An instance variables without a value gives nil
+      # It's used every time you cal the published method
+      # The method itself is basically the getter
+      @published ||= spots.published
     end
 
     def unpublished
-      @unpublished ||= spots.draft.group_by(&:city).sort_by(&:first)
+      @unpublished ||= spots.draft
+    end
+
+    def published_grouped_by_city
+      @published_grouped_by_city ||= group_by_city(@published)
+    end
+
+    def unpublished_grouped_by_city
+      @unpublished_grouped_by_city ||= group_by_city(@unpublished)
+    end
+
+    private
+
+    def group_by_city(published_and_unpublished_spots)
+      published_and_unpublished_spots.group_by(&:city).sort_by{ | city, _ | city.name }
     end
   end
 end
--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -1,27 +1,29 @@
 class ErrorsController < ApplicationController
   skip_before_action :authenticate_user!
 
+  # The instance variables get passed to the view and all the partials
+  # If using instance variables in a view is bad, then usign them in a partial is even worse
+  # Because lets imagine, that you use this partial somewhere else, take parts of your view and reuse them
+  # If you're using one view from two different controllers
   def not_found
-    @error_status = 404
-    @error_message = "So sorry, it looks like we broke this link. Or, you might've accidentally mispelled the url of the page you're trying to see."
-    respond_to do |format|
-      format.html { render status: 404 }
-    end
+    render_error(404, "So sorry, it looks like we broke this link. Or, you might've accidentally mispelled the url of the page you're trying to see.")
   end
 
   def unacceptable
-    @error_status = 422
-    @error_message = "So sorry, something went wrong. Please try something different."
-    respond_to do |format|
-      format.html { render status: 422 }
-    end
+    render_error(422, "So sorry, something went wrong. Please try something different.")
   end
 
   def internal_error
-    @error_status = 500
-    @error_message = "So sorry, it looks like we messed up. Please try again."
+    render_error(500, "So sorry, it looks like we messed up. Please try again.")
+  end
+
+  private
+
+  def render_error(status, message)
+    @error_status = status
+    @error_message = message
     respond_to do |format|
-      format.html { render status: 500 }
+      format.html { render status: status }
     end
   end
 end
