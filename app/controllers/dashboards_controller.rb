e4fb3306ef07e96a3d6b18c1a0922dd0d4f9dc7d
Mirha Masala <mirha.masala@me.com>
2020-02-13 14:52:48 +0800

    Fix dashboard method

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -26,11 +26,25 @@ class DashboardsController < ApplicationController
     end
 
     def published
-      @published ||= spots.published.group_by(&:city).sort_by(&:first)
+      @published ||= spots.published
+    end
+
+    def published_grouped_by_city
+      @published_grouped_by_city ||= group_by_city(@published)
     end
 
     def unpublished
-      @unpublished ||= spots.draft.group_by(&:city).sort_by(&:first)
+      @unpublished ||= spots.draft
+    end
+
+    def unpublished_grouped_by_city
+      @unpublished_grouped_by_city ||= group_by_city(@unpublished)
+    end
+
+    private
+
+    def group_by_city(published_and_unpublished_spots)
+      published_and_unpublished_spots.group_by(&:city).sort_by{ |city, _| city.name }
     end
   end
 end
