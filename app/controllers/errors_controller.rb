b8acce7c86ede2cf3930282093ba214508306e5f
Mirha Masala <mirha.masala@me.com>
2020-02-13 15:07:05 +0800

    Refactor error controller

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -2,26 +2,25 @@ class ErrorsController < ApplicationController
   skip_before_action :authenticate_user!
 
   def not_found
-    @error_status = 404
-    @error_message = "So sorry, it looks like we broke this link. Or, you might've accidentally mispelled the url of the page you're trying to see."
-    respond_to do |format|
-      format.html { render status: 404 }
-    end
+    render_error(404, "So sorry, it looks like we broke this link. Or, you might've accidentally mispelled the url of the page you're trying to see.")
   end
 
   def unacceptable
-    @error_status = 422
-    @error_message = "So sorry, something went wrong. Please try something different."
-    respond_to do |format|
-      format.html { render status: 422 }
-    end
+    render_error(422, "So sorry, something went wrong. Please try something different.")
   end
 
   def internal_error
-    @error_status = 500
-    @error_message = "So sorry, it looks like we messed up. Please try again."
+    render_error(500, "So sorry, it looks like we messed up. Please try again.")
+  end
+
+  private
+
+  def render_error(status, error_message)
+    @status = status
+    @error_message = error_message
+
     respond_to do |format|
-      format.html { render status: 500 }
+      format.html { render status: status }
     end
   end
 end
