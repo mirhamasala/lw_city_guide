1c8fa0a33cdc0c7945bd748b5d7ce61fd98ec66f
Mirha Masala <mirha.masala@me.com>
2020-03-11 13:44:03 +0800

    Refactor main svg + partials

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -2,15 +2,15 @@ class ErrorsController < ApplicationController
   skip_before_action :authenticate_user!
 
   def not_found
-    render_error(404, "So sorry, it looks like we broke this link. Or, you might've accidentally mispelled the url of the page you're trying to see.")
+    render_error(404, "So sorry, it looks like we derailed. Please let us take you home.")
   end
 
   def unacceptable
-    render_error(422, "So sorry, something went wrong. Please try something different.")
+    render_error(422, "So sorry, something went wrong. Please try something different or let us take you home.")
   end
 
   def internal_error
-    render_error(500, "So sorry, it looks like we messed up. Please try again.")
+    render_error(500, "So sorry, it looks like we messed up. Please try again or let us take you home.")
   end
 
   private
