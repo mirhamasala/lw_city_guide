55a548cdc39cd91105e1a4c1d2f39b49387d0d13
Mirha Masala <mirha.masala@me.com>
2020-04-03 13:49:47 +0800

    Remove old Javascripts folder from asset pipeline

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,5 +1,4 @@
 //= link_tree ../images
-//= link_directory ../javascripts .js
 //= link application_light.css
 //= link application_dark.css
 //= link_directory ../stylesheets .css
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,3 +1,4 @@
+require("@rails/ujs").start();
 require("turbolinks").start();
 
 // --------------------------------------------------------------
